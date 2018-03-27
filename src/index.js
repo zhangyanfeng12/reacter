c2bdab20f0bf5689c5cced8233ff21f9df497199
zhangyanfeng12 <873988914@qq.com>
2018-03-27 17:12:28 +0800

    appbar button

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import AppBar from './AppBar';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(<div><AppBar /></div>
+, document.getElementById('root'));
+
