24a414e617e4d56ac4d6776778db12b644c02c1c
zhangyanfeng12 <873988914@qq.com>
2018-03-28 09:29:37 +0800

    ClippedDrawer

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AppBar from './AppBar';
+import ClippedDrawer from './ClippedDrawer';
+import tileData from './tileData'
 
-
-ReactDOM.render(<div><AppBar /></div>
+ReactDOM.render(<div>
+    <AppBar />
+    <ClippedDrawer />
+    <tileData />
+</div>
 , document.getElementById('root'));
 
